38932d8a4ebb8c29acd0c5bf44b12c61d2a1e361
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2021-11-05 12:18:20 +0100

    Rename

--- DuckDefense/Program.cs
+++ DuckDefense/Program.cs
@@ -7,7 +7,7 @@ namespace DuckDefense
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GameWorld())
                 game.Run();
         }
     }
