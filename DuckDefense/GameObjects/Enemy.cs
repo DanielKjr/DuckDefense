dd1fd33388c193e4c44082ff67742b48109c6314
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2021-11-08 10:43:57 +0100

    Building classes and movement

---
+++ DuckDefense/GameObjects/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuckDefense
+{
+    class Enemy : GameObject
+    {
+        public override void LoadContent(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
