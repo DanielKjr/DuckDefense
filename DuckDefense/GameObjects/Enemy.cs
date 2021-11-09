a547c3e18d8f8213a6a452d7fdbaf45a7923600d
DanielKjr <93762921+DanielKjr@users.noreply.github.com>
2021-11-09 11:35:09 +0100

    Animation function and sort of movement but not really

--- DuckDefense/GameObjects/Enemy.cs
+++ DuckDefense/GameObjects/Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,17 +10,65 @@ namespace DuckDefense
     class Enemy : GameObject
     {
         private int health;
-        private int speed;
+       // private int speed;
 
+        public Enemy()
+        {
+           
 
+            velocity = new Vector2(1, 0);
+            speed = 100;
+            fps = 10;
+            this.position.X = 1260;
+            this.position.Y = 80;
+           
+            offset = Vector2.Zero;
+            color = Color.White;
+        }
+
+        /// <summary>
+        /// Loads the position and sprites to go through
+        /// </summary>
+        /// <param name="content"></param>
         public override void LoadContent(ContentManager content)
         {
-            throw new NotImplementedException();
+            sprites = new Texture2D[2];
+            sprites[0] = content.Load<Texture2D>("SpritePlaceHolder1");
+            sprites[1] = content.Load <Texture2D>("SpritePlaceHolder2");
+
+            
+
+            for (int i = 0; i < sprites.Length; i++)
+            {
+                sprites[i] = content.Load<Texture2D>("SpritePlaceHolder"+(i +1));
+            }
+
+            sprite = sprites[0];
+
+           // this.position = new Vector2(1260,80);
+            this.origin = new Vector2(0,0);
+            this.offset.X = (-sprite.Width / 2);
+            this.offset.Y = -sprite.Height / 2;
+
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            PathMovement();
+            Move(gameTime);
+            Animate(gameTime);
+            
+
+        }
+
+        public void PathMovement()
+        {
+            if (position.X != 140)
+            {
+                position.X--;
+            }
+          
+           
         }
     }
 }
--- DuckDefense/GameObjects/GameObject.cs
+++ DuckDefense/GameObjects/GameObject.cs
@@ -7,30 +7,78 @@ namespace DuckDefense
 {
 
 
-        abstract class GameObject
-     {
-        
+    abstract class GameObject
+    {
+
+        protected Vector2 position;
+        protected Vector2 velocity;
+        protected Vector2 offset;
+        protected Vector2 origin;
+
+        protected Texture2D[] sprites;
+        protected Texture2D sprite;
+
+        protected float fps;
+        private float timeElapsed;
+        protected float speed;
+        private int currentIndex;
+
+        protected Color color;
+
+        public Color GetColor
+        {
+            get { return color; }
+            set { color = value; }
+
+        }
 
 
 
 
 
+        public abstract void LoadContent(ContentManager content);
 
-               public abstract void LoadContent(ContentManager content);
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(sprite, position, null, color, 0, origin, 1, SpriteEffects.None, 0);
 
-                public void Draw(SpriteBatch spriteBatch)
-                {
-    
+        }
 
-                }
+        public abstract void Update(GameTime gameTime);
 
-                public abstract void Update(GameTime gameTime);
-          
+        /// <summary>
+        /// Animates by changing the sprites at the speed it is moving by (fps), velocity/speed/fps determines the speed of the animation
+        /// </summary>
+        /// <param name="gameTime"></param>
+        protected void Animate(GameTime gameTime)
+        {
+            timeElapsed += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            currentIndex = (int)(timeElapsed * fps);
+            sprite = sprites[currentIndex];
+
+            if (currentIndex >= sprites.Length -1)
+            {
+                timeElapsed = 0;
+                currentIndex = 0;
+            }
+
+        }
+
+        /// <summary>
+        /// Movement depending on the speed we determine 
+        /// </summary>
+        /// <param name="gameTime"></param>
+        protected void Move(GameTime gameTime)
+        {
+            float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            position += ((velocity * speed) * deltaTime);
+            
+        }
 
 
-           
-        
-     }
+
+    }
 
 
 }
